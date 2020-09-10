e5e27b188c58651d83e62072c8ad704572992012
t_seto_neton <t_seto@neton.co.jp>
2020-09-10 17:12:20 +0900

    pagination実装

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,60 @@
 <template>
   <v-app>
-    <header/>
+    <header />
     <v-main>
       <v-row>
-        <v-col v-for="content in contents" :key="content" cols="12">
-          <item
-            id="item"
-          ></item>
+        <v-col v-for="content in displayContents" :key="content.id" cols="12">
+          <p justify="center">{{ content.title }}</p>
         </v-col>
       </v-row>
 
-      <v-pagination
-        v-model="page"
-        :length="6"
-      ></v-pagination>
+      <v-pagination v-model="page" :length="5" @input="pagination"></v-pagination>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import Header from './components/Header.vue'
-import item from './components/Item.vue'
-import { PaginationStore } from './store/pagination'
-import { PaginationState } from './store/paginationTypes'
+import { Component, Vue } from "vue-property-decorator";
+import Header from "./components/Header.vue";
+import item from "./components/Item.vue";
+import _ from "lodash";
+
+interface Content {
+  id: number;
+  title: string;
+}
 
 @Component({
-  components:{
+  components: {
     Header,
-    item
-  }
+    item,
+  },
 })
-export default class App extends Vue{
-  private contents = [1,2,3,4];
+export default class App extends Vue {
+  private contents: Array<Content> = [{ id: 0, title: "" }];
+  private displayContents: Array<Content> = [];
+  private pageSize = 10;
   private page = 1;
+
+  mounted() {
+    this.contents = _.range(50)
+      .fill({ id: 0, title: "title" })
+      .map((item: Content, i: number) => {
+        return { id: i, title: "Title" + i };
+      });
+    this.displayContents = this.contents.slice(0, this.pageSize);
+  }
+
+  private pagination(page: number) {
+    this.displayContents = this.contents.slice(
+      this.pageSize * (page - 1),
+      this.pageSize * page
+    );
+  }
 }
 </script>
 
 <style scoped>
-  #item{
-    
-  }
+#item {
+}
 </style>
